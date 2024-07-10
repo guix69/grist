edbd01e1833ee8b9ace85c939dea26e8272bfb05
guix69 <guix@realguix.com>
2024-07-10 15:25:32 +0200

    Update page.js

--- map/page.js
+++ map/page.js
@@ -8,8 +8,9 @@ let selectedTableId = null;
 let selectedRowId = null;
 let selectedRecords = null;
 let mode = 'multi';
-let mapSource = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}';
-let mapCopyright = 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012';
+//let mapSource = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}';
+let mapSource = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+let mapCopyright = 'Openstreetmap';
 // Required, Label value
 const Name = "Name";
 // Required
